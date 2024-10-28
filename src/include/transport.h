de3ee93c07dabccbaf8b2ee1495f14b6508bad59
Nariaki Tateiwa <nariaki3551@gmail.com>
2024-10-28 15:04:00 +0900

    remove ncclTransportCheckP2pType from transport

--- src/include/transport.h
+++ src/include/transport.h
@@ -116,7 +116,6 @@ struct ncclTransport {
 
 ncclResult_t ncclTransportP2pConnect(struct ncclComm* comm, int channelId, int nrecv, int* peerRecv, int nsend, int* peerSend, int connIndex);
 ncclResult_t ncclTransportP2pSetup(struct ncclComm* comm, struct ncclTopoGraph* graph, int connIndex, int* highestTransportType=NULL);
-ncclResult_t ncclTransportCheckP2pType(struct ncclComm* comm, bool* intraNodeP2pSupport, bool* directMode);
 
 // ncclResult_t ncclNvlsInit(struct ncclComm* comm);
 // ncclResult_t ncclNvlsSetup(struct ncclComm* comm, struct ncclComm* parent);
